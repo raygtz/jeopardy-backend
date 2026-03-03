9ca9f929c2e8db6734bfdbb0d360b7b6cfdd0e6d
raygtz <raygtz22@gmail.com>
2026-03-02 23:03:25 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+let players = {};
+let roundActive = false;
+let winner = null;
+
+const ADMIN_PASSWORD = "admin123";
+
+io.on("connection", (socket) => {
+
+  socket.on("join", (name) => {
+    players[socket.id] = { name, points: 0 };
+    io.emit("updatePlayers", players);
+  });
+
+  socket.on("adminLogin", (pass, callback) => {
+    if (pass === ADMIN_PASSWORD) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+
+  socket.on("startRound", () => {
+    winner = null;
+    roundActive = true;
+    io.emit("countdown");
+  });
+
+  socket.on("buzz", () => {
+    if (roundActive && !winner) {
+      winner = players[socket.id].name;
+      roundActive = false;
+      io.emit("winner", winner);
+    }
+  });
+
+  socket.on("addPoint", (name) => {
+    for (let id in players) {
+      if (players[id].name === name) {
+        players[id].points++;
+      }
+    }
+    io.emit("updatePlayers", players);
+  });
+
+  socket.on("resetRound", () => {
+    winner = null;
+    roundActive = false;
+    io.emit("reset");
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("updatePlayers", players);
+  });
+
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor activo");
+});
